47334f171e350d117bad71ab161a8de29c9560ec
Elchi DIana <47711461+daimondy@users.noreply.github.com>
2020-04-19 17:12:23 +0300

    ура

--- Palindrome.java
+++ Palindrome.java
@@ -1,15 +1,22 @@
+import java.util.Scanner;
+
 public class Palindrome{
 	public static void main(String[] args){
-		Scanner in = new Scanner(System.in);    
+		Palindrome a = new Palindrome();
+		Scanner in = new Scanner(System.in);
 		System.out.println("Введите строку: ");
 		String s = in.nextLine();
-		in.close();
-		isPalindrome(s);
+		String w[] = s.split(" ");
+		for (String r:w){
+			a.isPalindrome(r);
+			System.out.println(s.equals(reversingString(s)));
+		}
 	}
 	public static String reversingString(String s){
 		String w = "";
-		for (int i = s.lenght()-1;i>=0;--i)
-			w +=s.charAt(i);
+		for (int i = 0; i < s.length(); i++){
+			w += s.charAt(s.length()-1-i);
+		}
 		return w;
 	}
 	public static boolean isPalindrome(String s){
