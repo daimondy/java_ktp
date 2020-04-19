e3cec4d7902d71515a2db9a00b99a99df67d7493
Elchi DIana <47711461+daimondy@users.noreply.github.com>
2020-04-19 17:32:42 +0300

    done with comms

--- lab1/Primes.java
+++ lab1/Primes.java
@@ -1,17 +1,17 @@
 public class Primes{
 	public static void main(String[] args){
 		for (int x = 2;x<100;x++){
-			if (isPrime(x) == true){
+			if (isPrime(x) == true){ //если число простое, то оно выводится
 				System.out.println(x);
 			}
 		}
 	}
 	public static boolean isPrime(int n){
 			for (int i = 2; i<n;i++){
-				if (n%i==0)
-					return false;
+				if (n%i==0) //проверка числа на делимость без остатка
+					return false; //если число делится без остатка, то оно не попадает в список
 			}
-			return true;
+			return true; //возвращает простые числа
 	}
 }
 
