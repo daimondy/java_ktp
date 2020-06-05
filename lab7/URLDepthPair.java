5ecd49aae881fec2c2f9c2479f61776bf12a122c
Elchi DIana <47711461+daimondy@users.noreply.github.com>
2020-06-05 23:39:49 +0300

    cfds

--- lab7/URLDepthPair.java
+++ lab7/URLDepthPair.java
@@ -2,38 +2,50 @@ import java.util.LinkedList;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+//Хранит URL-адреса и глубину, на которой они были исследованы
 public class URLDepthPair {
-    public static final String URL_PREFIX = "http://";
 
-    public String URL;
-    public int depth;
+	// Регулярное выражение для URL-адреса
+	public static final String URL_PREFIX = "http://";
 
-    public URLDepthPair (String URL, int depth){
-        this.URL=URL;
-        this.depth=depth;
-    }
+	public String URL;
 
+	public int depth;
 
-    public String getHost() throws MalformedURLException {
-        URL host = new URL(URL);
-        return host.getHost();
-    }
-    public String getPath() throws MalformedURLException {
-        URL path = new URL(URL);
-        return path.getPath();
-    }
-    public int getDepth() {
-        return depth;
-    }
-    public String getURL() {
-        return URL;
-    }
+	// Предполагает ввод URL-адрес абсолютный URL-адрес
+	public URLDepthPair (String URL, int depth){
+		this.URL=URL;
+		this.depth=depth;
+	}
 
-    public static boolean check(LinkedList<URLDepthPair> resultLink, URLDepthPair pair) {
-        boolean isAlready = true;
-        for (URLDepthPair c : resultLink)
-            if (c.getURL().equals(pair.getURL()))
-                isAlready=false;
-        return isAlready;
-    }
+	//Возвращает имя хоста сервера, указанное в URL-адресе
+	public String getHost() throws MalformedURLException {
+		URL host = new URL(URL);
+		return host.getHost();
+	}
+
+	//Возвращает ресурс на сервере
+	public String getPath() throws MalformedURLException {
+		URL path = new URL(URL);
+		return path.getPath();
+	}
+
+	// Возвращает глубину поиска URL-адреса 
+	public int getDepth() {
+		return depth;
+	}
+
+	//Возвращает URL-адрес пары 
+	public String getURL() {
+		return URL;
+	}
+
+	//Проверяет, является ли URL-адрес абсолютным URL-адресом, а не относительным
+	public static boolean check(LinkedList<URLDepthPair> resultLink, URLDepthPair pair) {
+		boolean isAlready = true;
+		for (URLDepthPair c : resultLink)
+			if (c.getURL().equals(pair.getURL()))
+				isAlready=false;
+		return isAlready;
+	}
 }
